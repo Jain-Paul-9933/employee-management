6a894502e2b3e6b9141d49d65c78ebeb21d1a56c
Jain-Paul-9933 <japs9933@gmail.com>
2025-09-13 22:25:45 +0530

    feat:added djoser package for authentication

--- backend/apps/authentication/models.py
+++ backend/apps/authentication/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+   email=models.EmailField(unique=True)
+   first_name=models.CharField(max_length=30,blank=True)
+   last_name=models.CharField(max_length=30,blank=True)
+   created_at=models.DateTimeField(auto_now_add=True)
+   updated_at=models.DateTimeField(auto_now=True)
+
+   REQUIRED_FIELDS = ['email','first_name','last_name']
+
+   def __str__(self):
+    return self.username
+   @property
+   def full_name(self):
+    return f"{self.first_name} {self.last_name}"
